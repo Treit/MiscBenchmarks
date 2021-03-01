23a0f0122307007bc7b3b62710b311c8d4a6f912
Mike Treit <mtreit@ntdev.microsoft.com>
2021-03-01 11:16:24 -0800

    Testing if for and foreach are equivalent for arrays.

---
+++ ArrayForVsForeach/Benchmark.cs
@@ -0,0 +1,65 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Diagnosers;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [MemoryDiagnoser]
+    [ShortRunJob]
+    public class Benchmark
+    {
+        private string[] _strings;
+
+        [Params(10, 100, 1000, 100_000, 1_000_000)]
+        public int Count { get; set; }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _strings = new string[Count];
+            for (int i = 0; i < Count; i++)
+            {
+                if (i % 10 == 0)
+                {
+                    _strings[i] = ("");
+                }
+                else
+                {
+                    _strings[i] = i.ToString();
+                }
+            }
+        }
+
+        [Benchmark]
+        public int ForLoopCount()
+        {
+            int count = 0;
+            for (int i = 0; i < _strings.Length; i++)
+            {
+                if (_strings[i].Length == 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        [Benchmark]
+        public int ForEachLoopCount()
+        {
+            int count = 0;
+            foreach (string s in _strings)
+            {
+                if (s.Length == 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
