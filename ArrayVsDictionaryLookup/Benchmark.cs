6b3719d39274e79f20aef5479edbda1534103398
Mike Treit <mtreit@ntdev.microsoft.com>
2021-03-02 10:54:15 -0800

    Array vs Dictionary lookup benchmark.

---
+++ ArrayVsDictionaryLookup/Benchmark.cs
@@ -0,0 +1,83 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Diagnosers;
+    using System.Collections.Generic;
+
+    public class SomeClass
+    {
+        public int DoSomething()
+        {
+            int total = 0;
+
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                total += i;
+            }
+
+            return total;
+        }
+    }
+
+    [MemoryDiagnoser]
+    [ShortRunJob]
+    public class Benchmark
+    {
+        [Params(1000, 100_000, 1_000_000)]
+        public int Iterations { get; set; }
+
+        private SomeClass[] arrayLookup;
+
+        private Dictionary<int, SomeClass> dictionaryLookup;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            int len = 50;
+
+            arrayLookup = new SomeClass[len];
+            dictionaryLookup = new Dictionary<int, SomeClass>(len);
+
+            for (int i = 0; i < len; i++)
+            {
+                arrayLookup[i] = new SomeClass();
+                dictionaryLookup.Add(i, new SomeClass());
+            }
+        }
+
+        [Benchmark]
+        public long LookupUsingArray()
+        {
+            long result = 0;
+
+            for (int i = 0; i < this.Iterations; i++)
+            {
+                for (int j = 0; j < arrayLookup.Length; i++)
+                {
+                    SomeClass c = arrayLookup[j];
+                    result += c.DoSomething();
+                }
+            }
+
+            return result;
+        }
+
+        [Benchmark]
+        public long LookupUsingDictionary()
+        {
+            long result = 0;
+
+            for (int i = 0; i < this.Iterations; i++)
+            {
+                for (int j = 0; j < arrayLookup.Length; i++)
+                {
+                    SomeClass c = dictionaryLookup[j];
+                    result += c.DoSomething();
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
