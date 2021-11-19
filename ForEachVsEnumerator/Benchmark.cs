b79102de26add3392c355c237faaf9f04751c5ce
Mike Treit <mtreit@ntdev.microsoft.com>
2021-11-19 14:02:19 -0800

    Add int64 tests.

--- ForEachVsEnumerator/Benchmark.cs
+++ ForEachVsEnumerator/Benchmark.cs
@@ -14,6 +14,9 @@
 
         private List<int> _data;
         private int[] _array;
+        private List<long> _data64;
+        private long[] _array64;
+
 
         [GlobalSetup]
         public void GlobalSetup()
@@ -126,6 +129,113 @@
             for (int i = 0; i < _array.Length; i++)
             {
                 int x = _array[i];
+
+                if (x > value)
+                {
+                    value = x;
+                }
+            }
+
+            return value;
+        }
+
+        [Benchmark]
+        public long MaxUsingEnumeratorList64()
+        {
+            long value;
+            using (IEnumerator<long> e = _data64.GetEnumerator())
+            {
+                value = e.Current;
+                while (e.MoveNext())
+                {
+                    long x = e.Current;
+                    if (x > value)
+                    {
+                        value = x;
+                    }
+                }
+            }
+
+            return value;
+        }
+
+        [Benchmark]
+        public long MaxUsingForEachList64()
+        {
+            long value = 0;
+
+            foreach (long x in _data64)
+            {
+                if (x > value)
+                {
+                    value = x;
+                }
+            }
+
+            return value;
+        }
+
+        [Benchmark]
+        public long MaxUsingForLoopList64()
+        {
+            long value = 0;
+
+            for (int i = 0; i < _data64.Count; i++)
+            {
+                long x = _data64[i];
+
+                if (x > value)
+                {
+                    value = x;
+                }
+            }
+
+            return value;
+        }
+
+        [Benchmark]
+        public long MaxUsingEnumeratorArray64()
+        {
+            long value = long.MinValue;
+            IEnumerator e = _array64.GetEnumerator();
+
+            while (e.MoveNext())
+            {
+                long x = (long)e.Current;
+
+                if (x > value)
+                {
+                    value = x;
+                }
+            }
+
+            return value;
+        }
+
+        [Benchmark]
+        public long MaxUsingForEachArray64()
+        {
+            long value = 0;
+
+            foreach (long x in _array64)
+            {
+                if (x > value)
+                {
+                    value = x;
+                }
+            }
+
+            return value;
+        }
+
+        [Benchmark]
+        public long MaxUsingForLoopArray64()
+        {
+            long value = 0;
+
+            for (int i = 0; i < _array64.Length; i++)
+            {
+                long x = _array64[i];
 
                 if (x > value)
                 {
