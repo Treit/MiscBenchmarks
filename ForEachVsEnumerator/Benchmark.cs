6d4f3449a2485c9ec741716ab220bee9c8f37212
Mike Treit <mtreit@ntdev.microsoft.com>
2021-11-18 19:05:52 -0800

    Add some variations.

--- ForEachVsEnumerator/Benchmark.cs
+++ ForEachVsEnumerator/Benchmark.cs
@@ -3,6 +3,7 @@
     using BenchmarkDotNet.Attributes;
     using BenchmarkDotNet.Diagnosers;
     using System;
+    using System.Collections;
     using System.Collections.Generic;
 
     [MemoryDiagnoser]
@@ -12,6 +13,7 @@
         public int Count { get; set; }
 
         private List<int> _data;
+        private int[] _array;
 
         [GlobalSetup]
         public void GlobalSetup()
@@ -23,11 +25,12 @@
             for (int i = 0; i < Count; i++)
             {
                 _data.Add(r.Next());
+                _array = _data.ToArray();
             }
         }
 
         [Benchmark]
-        public int MaxUsingEnumerator()
+        public int MaxUsingEnumeratorList()
         {
             int value;
             using (IEnumerator<int> e = _data.GetEnumerator())
@@ -47,12 +50,81 @@
         }
 
         [Benchmark(Baseline = true)]
-        public int MaxUsingForEach()
+        public int MaxUsingForEachList()
         {
             int value = 0;
 
             foreach (int x in _data)
             {
+                if (x > value)
+                {
+                    value = x;
+                }
+            }
+
+            return value;
+        }
+
+        [Benchmark]
+        public int MaxUsingForLoopList()
+        {
+            int value = 0;
+
+            for (int i = 0; i < _data.Count; i++)
+            {
+                int x = _data[i];
+
+                if (x > value)
+                {
+                    value = x;
+                }
+            }
+
+            return value;
+        }
+
+        [Benchmark]
+        public int MaxUsingEnumeratorArray()
+        {
+            int value;
+            IEnumerator e = _array.GetEnumerator();
+            value = (int)e.Current;
+            while (e.MoveNext())
+            {
+                int x = (int)e.Current;
+                if (x > value)
+                {
+                    value = x;
+                }
+            }
+
+            return value;
+        }
+
+        public int MaxUsingForEachArray()
+        {
+            int value = 0;
+
+            foreach (int x in _array)
+            {
+                if (x > value)
+                {
+                    value = x;
+                }
+            }
+
+            return value;
+        }
+
+        [Benchmark]
+        public int MaxUsingForLoopArray()
+        {
+            int value = 0;
+
+            for (int i = 0; i < _array.Length; i++)
+            {
+                int x = _array[i];
+
                 if (x > value)
                 {
                     value = x;
--- ForEachVsEnumerator/Program.cs
+++ ForEachVsEnumerator/Program.cs
@@ -12,7 +12,7 @@
             Benchmark b = new Benchmark();
             b.Count = 1000;
             b.GlobalSetup();
-            b.MaxUsingForEach();
+            b.MaxUsingForEachList();
 #endif
         }
     }
