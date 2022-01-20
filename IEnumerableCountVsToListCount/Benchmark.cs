f934ce1cf524d460b0519b63296f57907a733d20
Mike Treit <mtreit@ntdev.microsoft.com>
2022-01-20 11:20:46 -0800

    IEnumerable Count() vs ToList().Count

---
+++ IEnumerableCountVsToListCount/Benchmark.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace Test
+{
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Diagnosers;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        [Params(10, 1000, 100_000)]
+        public int Count { get; set; }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+        }
+
+        [Benchmark(Baseline = true)]
+        public int IEnumerableCount()
+        {
+            return Values().Count();
+        }
+
+        [Benchmark]
+        public int ToListCount()
+        {
+            return Values().ToList().Count;
+        }
+
+        private IEnumerable<int> Values()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}
+
+
