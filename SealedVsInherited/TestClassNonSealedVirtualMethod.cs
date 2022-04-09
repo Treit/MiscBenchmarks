d4170c4e41924f504111eaf30d1c9e8f0ab87c4d
Mike Treit <mtreit@ntdev.microsoft.com>
2022-04-09 16:12:37 -0700

    Sealed vs non-sealed classes.

---
+++ SealedVsInherited/TestClassNonSealedVirtualMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Test
+{
+    internal class TestClassNonSealedVirtualMethod
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public virtual long DoWork(int seed)
+        {
+            long result = 0;
+            var r = new Random(seed);
+
+            for (int i = 0; i < seed; i++)
+            {
+                result += r.Next(100_000);
+            }
+
+            return result;
+        }
+    }
+}
