ce941af1e90f89644e28dd790cc8775c02c85c66
Mike Treit <mtreit@ntdev.microsoft.com>
2022-04-20 16:13:09 -0700

    Update the tests.

--- RandomStrings/Benchmark.cs
+++ RandomStrings/Benchmark.cs
@@ -2,19 +2,28 @@
 {
     using BenchmarkDotNet.Attributes;
     using BenchmarkDotNet.Diagnosers;
+    using BenchmarkDotNet.Order;
     using System;
     using System.Linq;
     using System.Text;
 
     [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
     public class Benchmark
     {
-        [Params(10, 100, 1000)]
+        public static Random random;
+
+        [Params(100)]
         public int Count { get; set; }
+
+        [Params(100)]
+        public int MaxLen { get; set; }
+
 
         [GlobalSetup]
         public void GlobalSetup()
         {
+            random = new Random(Count);
         }
 
         [Benchmark(Baseline = true)]
@@ -25,7 +34,7 @@
 
             for (int i = 0; i < Count; i++)
             {
-                var s = GetRandomString(r, 50);
+                var s = GetRandomString(r, MaxLen);
                 result += s.Length;
             }
 
@@ -54,17 +63,18 @@
 
             for (int i = 0; i < Count; i++)
             {
-                var s = GenerateRandomString(50);
+                var s = GenerateRandomString(MaxLen);
                 result += s.Length;
             }
 
             return result;
 
-            string GenerateRandomString(int length)
+            string GenerateRandomString(int maxLength)
             {
+                var len = r.Next(maxLength);
                 Random rnd = new Random((int)(DateTime.Now.Ticks % int.MaxValue));
                 StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < length; i++)
+                for (int i = 0; i < len; i++)
                 {
                     sb.Append((char)rnd.Next(32, 126));//32-126 usable ascii characters
                 }
@@ -80,17 +90,135 @@
 
             for (int i = 0; i < Count; i++)
             {
-                var s = RandomString(r, 50);
+                var s = RandomString(r, MaxLen);
                 result += s.Length;
             }
 
             return result;
 
-            static string RandomString(Random random, int length)
+            static string RandomString(Random random, int maxLength)
             {
+                var len = random.Next(maxLength);
                 const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-                return new string(Enumerable.Repeat(chars, length)
+                return new string(Enumerable.Repeat(chars, len)
                     .Select(s => s[random.Next(s.Length)]).ToArray());
+            }
+        }
+
+        [Benchmark]
+        public long RandomStringsUsingStringDotCreate()
+        {
+            long result = 0;
+            byte[] buffer = new byte[MaxLen];
+            var r = new Random(Count);
+
+            for (int i = 0; i < Count; i++)
+            {
+                var s = RandomStringCreate(r, MaxLen);
+                result += s.Length;
+            }
+
+            return result;
+
+            static string RandomStringCreate(Random random, int maxLength)
+            {
+                var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_@!#$%^&*()+{}[]";
+
+                var len = random.Next(maxLength);
+                return string.Create(len, random, (buff, str) =>
+                {
+                    for (int i = 0; i < buff.Length; i++)
+                    {
+                        buff[i] = alphabet[random.Next(alphabet.Length)];
+                    }
+
+                });
+            }
+        }
+
+        [Benchmark]
+        public long RandomStringsUsingStringDotCreateConstAlpha()
+        {
+            long result = 0;
+            byte[] buffer = new byte[MaxLen];
+            var r = new Random(Count);
+
+            for (int i = 0; i < Count; i++)
+            {
+                var s = RandomStringCreate(r, MaxLen);
+                result += s.Length;
+            }
+
+            return result;
+
+            static string RandomStringCreate(Random random, int maxLength)
+            {
+                const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_@!#$%^&*()+{}[]";
+                var len = random.Next(maxLength);
+                return string.Create(len, random, (buff, str) =>
+                {
+                    for (int i = 0; i < buff.Length; i++)
+                    {
+                        buff[i] = alphabet[random.Next(alphabet.Length)];
+                    }
+                });
+            }
+        }
+
+        [Benchmark]
+        public long RandomStringsUsingStringDotCreateConstAlphaStaticRandom()
+        {
+            long result = 0;
+            byte[] buffer = new byte[MaxLen];
+
+            for (int i = 0; i < Count; i++)
+            {
+                var s = RandomStringCreate(MaxLen);
+                result += s.Length;
+            }
+
+            return result;
+
+            static string RandomStringCreate(int maxLength)
+            {
+                const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_@!#$%^&*()+{}[]";
+                var len = random.Next(maxLength);
+                return string.Create(len, random, (buff, str) =>
+                {
+                    for (int i = 0; i < buff.Length; i++)
+                    {
+                        buff[i] = alphabet[random.Next(alphabet.Length)];
+                    }
+                });
+            }
+        }
+
+        [Benchmark]
+        public long RandomStringAkari()
+        {
+            long result = 0;
+            var r = new Random(Count);
+
+            for (int i = 0; i < Count; i++)
+            {
+                var s = GetRandomString(r, MaxLen);
+                result += s.Length;
+            }
+
+            return result;
+            static string GetRandomString(Random r, int maxLength)
+            {
+                const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_@!#$%^&*()+{}[]";
+                unsafe
+                {
+                    char* str = stackalloc char[maxLength];
+                    for (var i = 0; i < maxLength; i++)
+                    {
+                        (*str++) = alphabet[r.Next(alphabet.Length)];
+                    }
+
+                    return new(str);
+                }
             }
         }
     }
--- RandomStrings/README.md
+++ RandomStrings/README.md
@@ -2,30 +2,20 @@
 
 ``` ini
 
-BenchmarkDotNet=v0.12.1, OS=Windows 10.0.22593
+BenchmarkDotNet=v0.12.1, OS=Windows 10.0.22598
 Intel Xeon W-2123 CPU 3.60GHz, 1 CPU, 8 logical and 4 physical cores
-.NET Core SDK=6.0.201
-  [Host]     : .NET Core 6.0.3 (CoreCLR 6.0.322.12309, CoreFX 6.0.322.12309), X64 RyuJIT
-  DefaultJob : .NET Core 6.0.3 (CoreCLR 6.0.322.12309, CoreFX 6.0.322.12309), X64 RyuJIT
+.NET Core SDK=6.0.202
+  [Host]     : .NET Core 6.0.4 (CoreCLR 6.0.422.16404, CoreFX 6.0.422.16404), X64 RyuJIT
+  DefaultJob : .NET Core 6.0.4 (CoreCLR 6.0.422.16404, CoreFX 6.0.422.16404), X64 RyuJIT
 
 
 ```
-|                                  Method | Count |              Mean |           Error |          StdDev | Ratio | RatioSD |  Gen 0 | Gen 1 | Gen 2 | Allocated |
-|---------------------------------------- |------ |------------------:|----------------:|----------------:|------:|--------:|-------:|------:|------:|----------:|
-|  **ForEachOfIListOfValueTupleBackedByList** |     **3** |        **198.580 ns** |       **3.9753 ns** |       **8.2979 ns** |  **1.00** |    **0.00** | **0.0334** |     **-** |     **-** |     **144 B** |
-|            ForEachOfArrayOfKeyValuePair |     3 |         10.718 ns |       0.2513 ns |       0.5673 ns |  0.05 |    0.00 |      - |     - |     - |         - |
-|              ForEachOfArrayOfValueTuple |     3 |          8.782 ns |       0.2139 ns |       0.4872 ns |  0.04 |    0.00 |      - |     - |     - |         - |
-| ForEachOfIListOfValueTupleBackedByArray |     3 |        129.962 ns |       2.6268 ns |       5.7104 ns |  0.65 |    0.04 | 0.0222 |     - |     - |      96 B |
-|       ForEachOfConcreteListOfValueTuple |     3 |         54.516 ns |       1.1100 ns |       1.2783 ns |  0.27 |    0.01 |      - |     - |     - |         - |
-|                                         |       |                   |                 |                 |       |         |        |       |       |           |
-|  **ForEachOfIListOfValueTupleBackedByList** |    **50** |     **38,917.509 ns** |     **765.4349 ns** |   **1,614.5620 ns** |  **1.00** |    **0.00** | **0.5493** |     **-** |     **-** |    **2400 B** |
-|            ForEachOfArrayOfKeyValuePair |    50 |      2,445.765 ns |      47.2771 ns |      93.3204 ns |  0.06 |    0.00 |      - |     - |     - |         - |
-|              ForEachOfArrayOfValueTuple |    50 |      1,635.628 ns |      32.4220 ns |      53.2703 ns |  0.04 |    0.00 |      - |     - |     - |         - |
-| ForEachOfIListOfValueTupleBackedByArray |    50 |     22,128.714 ns |     440.0294 ns |     898.8635 ns |  0.57 |    0.03 | 0.3662 |     - |     - |    1600 B |
-|       ForEachOfConcreteListOfValueTuple |    50 |      6,988.175 ns |     139.0958 ns |     194.9928 ns |  0.18 |    0.01 |      - |     - |     - |         - |
-|                                         |       |                   |                 |                 |       |         |        |       |       |           |
-|  **ForEachOfIListOfValueTupleBackedByList** |  **1000** | **13,451,759.149 ns** | **265,246.9286 ns** | **504,659.8486 ns** |  **1.00** |    **0.00** |      **-** |     **-** |     **-** |   **48008 B** |
-|            ForEachOfArrayOfKeyValuePair |  1000 |    911,129.289 ns |  17,874.9287 ns |  13,955.5686 ns |  0.07 |    0.00 |      - |     - |     - |       1 B |
-|              ForEachOfArrayOfValueTuple |  1000 |    656,567.383 ns |  12,826.9655 ns |  18,396.0506 ns |  0.05 |    0.00 |      - |     - |     - |         - |
-| ForEachOfIListOfValueTupleBackedByArray |  1000 |  8,693,049.253 ns | 167,763.0469 ns | 323,222.4367 ns |  0.65 |    0.03 |      - |     - |     - |   32008 B |
-|       ForEachOfConcreteListOfValueTuple |  1000 |  2,538,821.094 ns |  50,758.7337 ns | 111,416.6510 ns |  0.19 |    0.01 |      - |     - |     - |       2 B |
+|                                                  Method | Count | MaxLen |      Mean |    Error |   StdDev | Ratio | RatioSD |   Gen 0 | Gen 1 | Gen 2 | Allocated |
+|-------------------------------------------------------- |------ |------- |----------:|---------:|---------:|------:|--------:|--------:|------:|------:|----------:|
+| RandomStringsUsingStringDotCreateConstAlphaStaticRandom |   100 |    100 |  65.13 μs | 1.302 μs | 2.176 μs |  0.81 |    0.06 |  2.8076 |     - |     - |   12513 B |
+|             RandomStringsUsingStringDotCreateConstAlpha |   100 |    100 |  70.00 μs | 1.387 μs | 2.466 μs |  0.88 |    0.07 |  4.8828 |     - |     - |   21520 B |
+|                       RandomStringsUsingStringDotCreate |   100 |    100 |  72.18 μs | 1.372 μs | 2.474 μs |  0.90 |    0.04 |  5.1270 |     - |     - |   22320 B |
+|                         RandomStringsUsingStringBuilder |   100 |    100 |  78.81 μs | 1.519 μs | 4.080 μs |  1.00 |    0.00 |  6.9580 |     - |     - |   30048 B |
+|                   RandomStringsStackOverflowLinqVersion |   100 |    100 | 117.71 μs | 2.344 μs | 4.105 μs |  1.47 |    0.09 | 10.0098 |     - |     - |   43192 B |
+|                                       RandomStringAkari |   100 |    100 | 126.13 μs | 2.483 μs | 5.182 μs |  1.60 |    0.11 |       - |     - |     - |     304 B |
+|                                 RandomStringsTCMVersion |   100 |    100 | 259.25 μs | 5.017 μs | 7.958 μs |  3.21 |    0.20 | 18.0664 |     - |     - |   79288 B |
