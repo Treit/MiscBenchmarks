db6bbf5278fef207e7b3092a202fafa3865bdc73
Mike Treit <mtreit@ntdev.microsoft.com>
2022-07-05 14:08:19 -0700

    Test parsing problem from user Notro on Discord.

---
+++ RotoParsingProblem/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Running;
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if RELEASE
+            BenchmarkRunner.Run<Benchmark>();
+#else
+            Benchmark b = new Benchmark();
+            b.GlobalSetup();
+            var x = b.ParseUsingRegex();
+            var y = b.ParseUsingStateMachine();
+            var z = b.ParseUsingTokensViaSplit();
+
+            if (x != y || y != z)
+            {
+                throw new InvalidOperationException("Expected everything to be equal");
+            }
+#endif
+
+        }
+    }
+}
