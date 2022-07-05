22900001801b386fceb78b496bf1e937bd1fff47
Fredric Silberberg <fred@silberberg.xyz>
2022-07-04 22:27:48 -0700

    Add benchmark for string equality comparisons.

---
+++ StringEquals/Program.cs
@@ -0,0 +1,57 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<StringTest>();
+
+public class StringTest
+{
+    readonly string string1 = new string('a', 10);
+    readonly string string2 = new string('a', 10);
+
+    [Params(10, 100)]
+    public int numIterations;
+
+    [Benchmark]
+    public bool EqualsObject()
+    {
+        bool result = false;
+        for (int i = 0; i < numIterations; i++)
+        {
+            result = string1.Equals((object)string2);
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public bool EqualsString()
+    {
+        bool result = false;
+        for (int i = 0; i < numIterations; i++)
+        {
+            result = string1.Equals(string2);
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public bool EqualsStringExplicitOrdinal()
+    {
+        bool result = false;
+        for (int i = 0; i < numIterations; i++)
+        {
+            result = string1.Equals(string2, StringComparison.Ordinal);
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public bool EqualsStringOperator()
+    {
+        bool result = false;
+        for (int i = 0; i < numIterations; i++)
+        {
+            result = string1 == string2;
+        }
+        return result;
+    }
+}
