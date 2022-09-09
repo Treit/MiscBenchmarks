23ab06418d647e1d7b28b21e834bc53deb7d7241
Mike Treit <mtreit@ntdev.microsoft.com>
2022-09-09 14:46:17 -0700

    Making HashSets.

---
+++ ToHashSet/Benchmark.cs
@@ -0,0 +1,44 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Diagnosers;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        [Params(100, 10_000)]
+        public int Count { get; set; }
+
+        List<string> _values = new();
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                _values.Add(Guid.NewGuid().ToString());
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public HashSet<string> NewHashSet()
+        {
+            return new HashSet<string>(_values);
+        }
+
+        [Benchmark]
+        public HashSet<string> LinqToHashSet()
+        {
+            return _values.ToHashSet();
+        }
+
+        [Benchmark]
+        public HashSet<string> LinqToImmutableHashSet()
+        {
+            return _values.ToHashSet();
+        }
+    }
+}
---
+++ ToHashSet/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Running;
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if RELEASE
+            BenchmarkRunner.Run<Benchmark>();
+#else
+            Benchmark b = new Benchmark();
+            b.Count = 1000;
+            b.GlobalSetup();
+            b.NewHashSe();
+#endif
+        }
+    }
+}
