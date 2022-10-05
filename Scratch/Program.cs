6dd001038ef65636dc6ccfba8dcfa5197084d932
Mike Treit <mtreit@ntdev.microsoft.com>
2022-10-05 15:51:37 -0700

    Scratchpad benchmark for testing things out.

---
+++ Scratch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Test
+{
+    public class PerfTest
+    {
+        [Params(4, 8)]
+        public int PredicateCount { get; set; }
+
+        [Benchmark]
+        public void Test()
+        {
+            IEnumerable<int> rows = new int[] { 1234 };
+            var predicates = new Func<int, bool>[PredicateCount];
+
+            for (int i = 0; i < PredicateCount; i++)
+            {
+                predicates[i] = x => true;
+            }
+
+            foreach (var predicate in predicates)
+            {
+                rows = rows.AsParallel().Where(predicate);
+            }
+
+            var x = rows.ToList();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
