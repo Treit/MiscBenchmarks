3cfb7341b3c25d90a2e655396cd17a0bf72b074f
Mike Treit <mtreit@ntdev.microsoft.com>
2023-02-22 17:00:21 -0800

    HashSet<T> vs. C5 HashSet<T>

---
+++ HashSetVsC5HashSet/Benchmark.cs
@@ -0,0 +1,44 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Diagnosers;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        [Params(100, 1_000_000)]
+        public int Count { get; set; }
+
+        private int[] _array;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            Random r = new Random();
+
+            _array = new int[Count];
+
+            for (int i = 0; i < Count; i++)
+            {
+                _array[i] = r.Next(0, 100);
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public int[] DedupeUsingHashSet()
+        {
+            return new HashSet<int>(_array).ToArray();
+        }
+
+        [Benchmark]
+        public int[] DedupeUsingC5HashSet()
+        {
+            var c5hashSet = new C5.HashSet<int>(Count, 0.6, EqualityComparer<int>.Default);
+            c5hashSet.AddAll(_array);
+            return c5hashSet.ToArray();
+        }
+    }
+}
---
+++ HashSetVsC5HashSet/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Running;
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if RELEASE
+            BenchmarkRunner.Run<Benchmark>();
+#else
+            Benchmark b = new Benchmark();
+            b.Count = 1024;
+            b.GlobalSetup();
+            var x = b.DedupeUsingHashSet().OrderBy(x => x).ToArray();
+            b.GlobalSetup();
+            var y = b.DedupeUsingC5HashSet().OrderBy(x => x).ToArray();
+
+            Console.WriteLine(x.AsSpan().SequenceEqual(y));
+#endif
+
+        }
+    }
+}
