06843631f4bb7c43288fd5875a6fa65ee44c3285
Mike Treit <mtreit@ntdev.microsoft.com>
2023-08-30 17:13:10 -0700

    Distinct count of lists

---
+++ DistinctToList/Benchmark.cs
@@ -0,0 +1,52 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Attributes;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        [Params(10, 1_000_000)]
+        public int Count { get; set; }
+
+        private List<int> _data;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _data = new List<int>(Count);
+
+            var r = new Random(Count);
+
+            for (int i = 0; i < Count; i++)
+            {
+                _data.Add(i);
+
+                if (r.Next() % 2 == 0)
+                {
+                    _data.Add(i);
+                }
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public long ToListThenDistinct()
+        {
+            return _data.ToList().Distinct().Count();
+        }
+
+        [Benchmark]
+        public long DistinctThenToList()
+        {
+            return _data.Distinct().ToList().Count();
+        }
+
+        [Benchmark]
+        public long DistinctOnly()
+        {
+            return _data.Distinct().Count();
+        }
+    }
+}
---
+++ DistinctToList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Test
+{
+    using BenchmarkDotNet.Running;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if RELEASE
+            BenchmarkRunner.Run<Benchmark>();
+#else
+            Benchmark b = new Benchmark();
+            b.Count = 1000;
+            b.GlobalSetup();
+            Console.WriteLine(b.ToListThenDistinct());
+            Console.WriteLine(b.DistinctThenToList());
+            Console.WriteLine(b.DistinctOnly());
+#endif
+
+        }
+    }
+}
