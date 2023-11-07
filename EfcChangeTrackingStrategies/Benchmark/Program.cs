acd8eda2dad7122cdef4207e59121a872d186e0f
almightylks <almightylks@protonmail.com>
2023-11-07 23:36:45 +0100

    Standard Program.cs Benchmark lines

--- EfcChangeTrackingStrategies/Benchmark/Program.cs
+++ EfcChangeTrackingStrategies/Benchmark/Program.cs
@@ -1,32 +1,5 @@
-﻿using BenchmarkDotNet.Analysers;
-using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Exporters;
-using BenchmarkDotNet.Loggers;
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
+using Test;
 
-namespace Test;
-
-internal class Program
-{
-    static void Main(string[] args)
-    {
-        var config = new ManualConfig();
-        config.AddColumnProvider(DefaultConfig.Instance.GetColumnProviders().ToArray());
-        config.AddExporter(DefaultConfig.Instance.GetExporters().ToArray());
-        config.AddDiagnoser(DefaultConfig.Instance.GetDiagnosers().ToArray());
-        config.AddAnalyser(DefaultConfig.Instance.GetAnalysers().ToArray());
-        config.AddJob(DefaultConfig.Instance.GetJobs().ToArray());
-        config.AddValidator(DefaultConfig.Instance.GetValidators().ToArray());
-        config.UnionRule = ConfigUnionRule.AlwaysUseGlobal; // Overriding the default
-
-        var smallSummary = BenchmarkRunner.Run<SmallBenchmark>(config);
-        var bigSummary = BenchmarkRunner.Run<BigBenchmark>(config);
-
-        var logger = ConsoleLogger.Default;
-        MarkdownExporter.Console.ExportToLog(smallSummary, logger);
-        ConclusionHelper.Print(logger, smallSummary.BenchmarksCases.First().Config.GetCompositeAnalyser().Analyse(smallSummary).ToList());
-
-        MarkdownExporter.Console.ExportToLog(bigSummary, logger);
-        ConclusionHelper.Print(logger, bigSummary.BenchmarksCases.First().Config.GetCompositeAnalyser().Analyse(bigSummary).ToList());
-    }
-}
+BenchmarkRunner.Run<SmallBenchmark>();
+BenchmarkRunner.Run<BigBenchmark>();
