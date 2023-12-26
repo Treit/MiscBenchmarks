dd23c475fc9f6ac5c5085c97cb15f52504e94d57
Mike Treit <mtreit@ntdev.microsoft.com>
2023-12-26 13:17:09 -0800

    Correct that it's an array not a list.

---
+++ MaxOfIntArray/Benchmark.cs
@@ -0,0 +1,46 @@
+﻿namespace Test;
+
+using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+[MemoryDiagnoser]
+public class Benchmark
+{
+    [Params(1_000, 100_000_000)]
+    public int Count { get; set; } = 100_000;
+
+    Random ran;
+    int[] _numbers;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        ran = new Random(1);
+        _numbers = new int[Count];
+        for (var i = 0; i < Count; i++)
+        {
+            _numbers[i] = ran.Next();
+        }
+    }
+
+    [Benchmark(Baseline = true)]
+    public int MaxWithLoop()
+    {
+        var result = int.MinValue;
+        foreach (var n in _numbers)
+        {
+            result = Math.Max(n, result);
+        }
+
+        return result;
+    }
+
+    [Benchmark]
+    public int EnumerableMax()
+    {
+        IEnumerable<int> numbers = _numbers;
+        return numbers.Max();
+    }
+}
