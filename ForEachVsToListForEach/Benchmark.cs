09ab5a3051c55f660bed16124fb9f09d9408ea93
Mike Treit <mtreit@ntdev.microsoft.com>
2024-01-27 15:37:45 -0800

    foreach vs. ToList().ForEach

---
+++ ForEachVsToListForEach/Benchmark.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Test
+{
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        private IEnumerable<int> _values;
+
+        [Params(1000, 1_000_000)]
+        public int Count { get; set; }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            var tempList = new List<int>(Count);
+            for (int i = 0; i < Count;i++ )
+            {
+                tempList.Add(Random.Shared.Next(0, 500_001));
+            }
+
+            _values = tempList.Where(x => x > 50_000);
+        }
+
+        [Benchmark(Baseline = true)]
+        public long ForEach()
+        {
+            var result = 0L;
+            foreach (var val in _values)
+            {
+                result += val;
+            }
+
+            return result;
+        }
+
+        [Benchmark]
+        public long ToListDotForEach()
+        {
+            var result = 0L;
+            _values.ToList().ForEach(x => result += x);
+            return result;
+        }
+    }
+}
---
+++ ForEachVsToListForEach/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using Test;
+
+#if RELEASE
+BenchmarkRunner.Run<Benchmark>();
+#else
+
+var b = new Benchmark();
+b.Count = 1_000_000;
+b.GlobalSetup();
+var first = b.ForEach();
+var second = b.ToListDotForEach();
+Console.WriteLine($"{first} <-> {second}");
+#endif
