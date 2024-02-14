4924a0c1498084a03cf6fca67431cb16891fb830
Mike Treit <mtreit@ntdev.microsoft.com>
2024-02-14 12:12:08 -0800

    Updates.

---
+++ HexStringToBytes/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Running;
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if RELEASE
+            BenchmarkRunner.Run<Benchmark>();
+#else
+            Benchmark b = new Benchmark();
+            b.HexStringLength = 10;
+            b.GlobalSetup();
+            var resultA = b.HexStringToBytesUsingConvert();
+            var resultB = b.HexStringToBytesUsingCustomImplementation();
+            Console.WriteLine(resultA.SequenceEqual(resultB));
+            Console.WriteLine(Convert.ToHexString(resultA));
+#endif
+        }
+    }
+}
