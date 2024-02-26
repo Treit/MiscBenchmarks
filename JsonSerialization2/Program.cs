504635e7dcdd61ac5ea0eb42a88a1f2ea7df1ec3
Mike Treit <mtreit@ntdev.microsoft.com>
2024-02-26 14:52:23 -0800

    Update results.

---
+++ JsonSerialization2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Running;
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if RELEASE
+            BenchmarkRunner.Run<Benchmark>();
+#else
+            Benchmark b = new Benchmark();
+            b.Count = 1000;
+            b.GlobalSetup();
+            var first = b.SerializeNewtonsoft();
+            var second = b.SerializeSTJ();
+            Console.WriteLine($"First: {first}, Second: {second}");
+#endif
+
+        }
+    }
+}
