880419fc69275a38cdbbcfb7887c1d3815d08a9c
Mike Treit <mtreit@ntdev.microsoft.com>
2024-03-04 17:25:33 -0800

    Removing tasks from a set as they finish.

---
+++ TaskWhenAnyRemove/Benchmark.cs
@@ -0,0 +1,58 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Diagnosers;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+        }
+
+        [Benchmark(Baseline = true)]
+        public async Task TaskWhenAnyRemoveWithHashSet()
+        {
+            var tasks = new HashSet<Task<float>>();
+            for (int i = 0; i < 10; i++)
+            {
+                var task = Task.Run(() => SimulateWork());
+                tasks.Add(task);
+            }
+
+            while (tasks.Any()) 
+            {
+                var completedTask = await Task.WhenAny(tasks);
+                tasks.Remove(completedTask);
+            }            
+        }
+
+        [Benchmark]
+        public async Task TaskWhenAnyRemoveWithList()
+        {
+            var tasks = new List<Task<float>>();
+            for (int i = 0; i < 10; i++)
+            {
+                var task = Task.Run(() => SimulateWork());
+                tasks.Add(task);
+            }
+
+            while (tasks.Any()) 
+            {
+                var completedTask = await Task.WhenAny(tasks);
+                tasks.Remove(completedTask);
+            }            
+        }        
+
+        private static async Task<float> SimulateWork()
+        {
+            await Task.Yield();
+            return 0.0f;
+        }
+    }
+}
