319d22efacdb157d33c03e3454f01ee7963fdff2
Mike Treit <mtreit@ntdev.microsoft.com>
2024-03-13 13:47:28 -0700

    Searching for multiple possible values in a list of strings.

---
+++ SearchStringListForMultipleValues/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Running;
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if RELEASE
+            BenchmarkRunner.Run<Benchmark>();
+#else
+            Benchmark b = new Benchmark();
+            b.Count = 1000;
+            b.GlobalSetup();
+            var first = b.CheckWithForEachSinglePass();
+            var second = b.CheckWithMultipleContainsCalls();
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+#endif
+        }
+    }
+}
