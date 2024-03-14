0849bf73e5575c6dda69aaae6a5057b7c1fd99c2
Mike Treit <mtreit@ntdev.microsoft.com>
2024-03-14 10:36:49 -0700

    Checking if strings are the same using StartsWith vs. Equals.

---
+++ StringStartsWithVsEquals/Benchmark.cs
@@ -0,0 +1,117 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Attributes;
+    using System;
+
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        private (string, string)[] _stringPairs;
+
+        [Params(100)]
+        public int Count { get; set; }
+
+        [Params(3, 100)]
+        public int StringLength { get; set; }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _stringPairs = new (string, string)[Count];
+            var random = new Random(Count);
+
+            for (int i = 0; i < Count; i++)
+            {
+                var strA = RandomStringCreate(random, StringLength);
+                var strB = (random.Next() % 2) switch
+                {
+                    0 => strA,
+                    _ => RandomStringCreate(random, StringLength)
+                };
+
+                _stringPairs[i] = (strA, strB);
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public int StartsWithOrdinalIgnoreCase()
+        {
+            var pairs = _stringPairs;
+            var result = 0;
+
+            foreach (var (first, second) in pairs)
+            {
+                if (first.StartsWith(second, StringComparison.OrdinalIgnoreCase))
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+
+        [Benchmark]
+        public int EqualsOrdinalIgnoreCase()
+        {
+            var pairs = _stringPairs;
+            var result = 0;
+
+            foreach (var (first, second) in pairs)
+            {
+                if (first.Equals(second, StringComparison.OrdinalIgnoreCase))
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+
+        [Benchmark]
+        public int AsSpanEqualsOrdinalIgnoreCase()
+        {
+            var pairs = _stringPairs;
+            var result = 0;
+
+            foreach (var (first, second) in pairs)
+            {
+                if (first.AsSpan(0, StringLength).Equals(second, StringComparison.OrdinalIgnoreCase))
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+
+        [Benchmark]
+        public int AsSpanStartsWithOrdinalIgnoreCase()
+        {
+            var pairs = _stringPairs;
+            var result = 0;
+
+            foreach (var (first, second) in pairs)
+            {
+                if (first.AsSpan(0, StringLength).StartsWith(second, StringComparison.OrdinalIgnoreCase))
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+
+        static string RandomStringCreate(Random random, int length)
+        {
+            const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_@!#$%^&*()+{}[]";
+
+            return string.Create(length, random, (buff, str) =>
+            {
+                for (int i = 0; i < buff.Length; i++)
+                {
+                    buff[i] = alphabet[random.Next(alphabet.Length)];
+                }
+            });
+        }
+    }
+}
---
+++ StringStartsWithVsEquals/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Running;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if RELEASE
+            BenchmarkRunner.Run<Benchmark>();
+#else
+            var b = new Benchmark();
+            b.Count = 100;
+            b.StringLength = 3;
+            b.GlobalSetup();
+
+#endif
+        }
+    }
+}
