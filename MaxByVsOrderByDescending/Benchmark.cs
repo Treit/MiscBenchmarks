766b1fe5b8b0851a085a91848d67e4ae97cced74
Mike Treit <mtreit@ntdev.microsoft.com>
2024-03-20 11:40:56 -0700

    MaxBy() vs. OrderByDescending().First()

---
+++ MaxByVsOrderByDescending/Benchmark.cs
@@ -0,0 +1,44 @@
+﻿namespace Test;
+
+using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+public record MyClass(int Id, string Name);
+
+[MemoryDiagnoser]
+public class Benchmark
+{
+    List<MyClass> _data;
+
+    [Params(100, 100_000)]
+    public int Count { get; set; } = 100_000;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _data = new List<MyClass>(Count);
+        var random = new Random(Count);
+
+        for (var i = 0; i < Count; i++)
+        {
+            var id = random.Next();
+            var name = Guid.NewGuid().ToString();
+            _data.Add(new MyClass(id, name));
+        }
+    }
+
+    [Benchmark(Baseline = true)]
+    public MyClass MaxBy()
+    {
+        return _data.MaxBy(x => x.Id);
+    }
+
+    [Benchmark]
+    public MyClass OrderByDescendingFirst()
+    {
+        return _data.OrderByDescending(x => x.Id).FirstOrDefault();
+    }
+}
