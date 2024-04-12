3532a98460106e86d2f118d40dac8931e4a301eb
Mike Treit <mtreit@ntdev.microsoft.com>
2024-04-12 10:14:01 -0700

    Deduping a delimited string.

---
+++ DedupeDelimitedString/Benchmark.cs
@@ -0,0 +1,72 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Diagnosers;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        private string _delimitedString;
+
+        [Params(10, 1000)]
+        public int Count;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < Count; i++)
+            {
+                sb.Append(i);
+
+                if (i < Count - 1)
+                {
+                    sb.Append(',');
+                }
+            }
+
+            _delimitedString = sb.ToString();
+        }
+
+        [Benchmark]
+        public int DedupeWithRemoveDuplicateFunction()
+        {
+            var deduped = RemoveDuplicates(_delimitedString).Split(',');
+
+            static string RemoveDuplicates(string nameList)
+            {
+                var hashSet = new HashSet<string>();
+                var result = nameList;
+
+                if (!string.IsNullOrWhiteSpace(nameList))
+                {
+                    foreach (var name in nameList.Split(','))
+                    {
+                        hashSet.Add(name.Trim());
+                    }
+                    result = string.Join(",", hashSet);
+                }
+
+                return result;
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public int DedupeWithLinqDistinct()
+        {
+            var result = 0;
+            var deduped = _delimitedString.Split(',').Select(x => x.Trim()).Distinct();
+
+            foreach (var item in deduped)
+            {
+                result += item.Length;
+            }
+
+            return result;
+        }
+    }
+}
