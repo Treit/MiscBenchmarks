f149caea2b1abed79a3dc88a7b2754f8321a52b1
Randy Treit <randyt@microsoft.com>
2024-04-26 14:52:10 -0700

    Add WordToMarkdownConverter

---
+++ WordToMarkdownConverter/Benchmark.cs
@@ -0,0 +1,117 @@
+namespace Test
+{
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Diagnosers;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Threading.Tasks;
+
+
+    [ShortRunJob]
+    public class Benchmark
+    {
+
+        private static string s_targetDir = @"c:\temp\andy";
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            // delete all markdown files in the target directory
+            string[] files = Directory.GetFiles(s_targetDir, "*.md");
+            foreach (string file in files)
+            {
+                File.Delete(file);
+            }
+        }
+
+        bool ConvertToMarkdown(string inputFile, string outputFile)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "pandoc",
+                Arguments = $"\"{inputFile}\" -t markdown -o \"{outputFile}\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+
+            using (Process process = Process.Start(startInfo)!)
+            {
+                process.WaitForExit();
+                if (process.ExitCode == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to convert {inputFile}");
+                    return false;
+                }
+            }
+        }
+        public async Task<bool> ConvertToMarkdownAsync(string inputFile, string outputFile)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "pandoc",
+                Arguments = $"\"{inputFile}\" -t markdown -o \"{outputFile}\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+
+            using (Process process = Process.Start(startInfo)!)
+            {
+                await process.WaitForExitAsync();
+                if (process.ExitCode == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to convert {inputFile}");
+                    return false;
+                }
+            }
+        }
+        [Benchmark(Baseline = true)]
+        public bool ConvertPandocParrallelForEach()
+        {
+            string[] files = Directory.GetFiles(s_targetDir, "*.docx");
+
+            Parallel.ForEach(files, file =>
+            {
+                string markdownFile = Path.ChangeExtension(file, ".md");
+                ConvertToMarkdown(file, markdownFile);
+            });
+            return true;
+        }
+        [Benchmark]
+        public async Task<bool> ConvertPandocParrallelForEachAsync()
+        {
+            string[] files = Directory.GetFiles(s_targetDir, "*.docx");
+
+            await Parallel.ForEachAsync(files, async (file, _) =>
+            {
+                string markdownFile = Path.ChangeExtension(file, ".md");
+                await ConvertToMarkdownAsync(file, markdownFile);
+            });
+            return true;
+        }        
+
+         [Benchmark]
+        public bool ConvertPandocForEach()
+        {
+            string[] files = Directory.GetFiles(s_targetDir, "*.docx");
+
+            foreach (string file in files)
+            {
+                string markdownFile = Path.ChangeExtension(file, ".md");
+                ConvertToMarkdown(file, markdownFile);
+            };
+            return true;
+        }
+    }
+}
