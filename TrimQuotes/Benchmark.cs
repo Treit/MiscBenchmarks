50f4eded8b11b3ee0d5dfc032344f00282aff94b
Mike Treit <mtreit@ntdev.microsoft.com>
2024-06-07 18:22:27 -0700

    Removing quotes from the start and end of a quoted string.

---
+++ TrimQuotes/Benchmark.cs
@@ -0,0 +1,81 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Attributes;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Benchmark
+    {
+        private List<string> _strings;
+
+        [Params(100, 100_000)]
+        public int Count { get; set; }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _strings = new List<string>(Count);
+            for (int i = 0; i < Count; i++)
+            {
+                _strings.Add($"\"{i}\"");
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public string DeQuoteWithTrim()
+        {
+            var result = "";
+
+            for (int i = 0; i < _strings.Count; i++)
+            {
+                var newStr = _strings[i].Trim('"');
+                result = newStr;
+            }
+
+            return result;
+        }
+
+        [Benchmark]
+        public string DeQuoteWithSubstring()
+        {
+            var result = "";
+
+            for (int i = 0; i < _strings.Count; i++)
+            {
+                var newStr = _strings[i].Substring(1, _strings[i].Length - 2);
+                result = newStr;
+            }
+
+            return result;
+        }
+
+        [Benchmark]
+        public string DeQuoteWithRangePattern()
+        {
+            var result = "";
+
+            for (int i = 0; i < _strings.Count; i++)
+            {
+                var newStr = _strings[i][1..^1];
+                result = newStr;
+            }
+
+            return result;
+        }
+
+        [Benchmark]
+        public string DeQuoteWithSpan()
+        {
+            var result = "";
+
+            for (int i = 0; i < _strings.Count; i++)
+            {
+                var span = _strings[i].AsSpan().Slice(1, _strings[i].Length - 2);
+                result = new string(span);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TrimQuotes/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Running;
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if RELEASE
+            BenchmarkRunner.Run<Benchmark>();
+#else
+            Benchmark benchmark = new Benchmark();
+            benchmark.Count = 1000;
+            benchmark.GlobalSetup();
+            var first = benchmark.DeQuoteWithTrim();
+            var second = benchmark.DeQuoteWithSubstring();
+            var third = benchmark.DeQuoteWithRangePattern();
+            var fourth = benchmark.DeQuoteWithSpan();
+            Console.WriteLine($"DeQuoteWithTrim: {first}");
+            Console.WriteLine($"DeQuoteWithSubstring: {second}");
+            Console.WriteLine($"DeQuoteWithRangePattern: {third}");
+            Console.WriteLine($"DeQuoteWithSpan: {fourth}");
+#endif
+
+        }
+    }
+}
