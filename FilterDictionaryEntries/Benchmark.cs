9d82e5a1dcbb726fd883bd6245d6d303cf006034
Mike Treit (from Dev Box) <mtreit@ntdev.microsoft.com>
2024-12-13 16:49:15 -0800

    Filtering dictionary entries

---
+++ FilterDictionaryEntries/Benchmark.cs
@@ -0,0 +1,76 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Attributes;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public record Instrument(int InstrumentId, string InstrumentName);
+    public record Profile(
+        string Value,
+        bool IsLowConfidence,
+        bool IsOfInterest,
+        bool IsLowLevel,
+        bool IsFlagged);
+
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        private List<Instrument> _candidates;
+        private Dictionary<int, Profile> _profiles;
+
+        [Params(100, 10_000)]
+        public int Count { get; set; }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _candidates = new List<Instrument>(Count);
+            var random = new Random(Count);
+
+            for (int i = 0; i < Count; i++)
+            {
+                _candidates.Add(new Instrument(i, $"Instrument {i}"));
+            }
+
+            _profiles = new Dictionary<int, Profile>(Count);
+            for (int i = 0; i < Count; i++)
+            {
+                _profiles.Add(i, new Profile(
+                    $"Profile {i}",
+                    true,
+                    random.Next(0, 10) == 0,
+                    random.Next(0, 10) == 0,
+                    random.Next(0, 10) == 0));
+            }
+        }
+
+        [Benchmark]
+        public Instrument[] FilterUsingConvolutedLinq()
+        {
+            var validCandidates = _candidates
+                                   .Where(p => _profiles
+                                   .Where(kvp => !kvp.Value.IsLowConfidence
+                                   || (kvp.Value.IsLowConfidence && kvp.Value.IsOfInterest)
+                                   || (kvp.Value.IsLowConfidence && kvp.Value.IsLowLevel)
+                                   || (kvp.Value.IsLowConfidence && kvp.Value.IsFlagged))
+                                   .Select(d => d.Key).ToList().Contains(p.InstrumentId)).ToArray();
+
+            return validCandidates;
+        }
+
+        [Benchmark(Baseline = true)]
+        public Instrument[] FilterUsingCleanedUpPatternMatching()
+        {
+            var validCandidates = _candidates.Where(
+                    p => _profiles.TryGetValue(p.InstrumentId, out var item)
+                    && item is
+                    { IsLowConfidence: false } or
+                    { IsOfInterest: true } or
+                    { IsLowLevel: true } or
+                    { IsFlagged: true }).ToArray();
+
+            return validCandidates;
+        }
+    }
+}
---
+++ FilterDictionaryEntries/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Running;
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if RELEASE
+            BenchmarkRunner.Run<Benchmark>();
+#else
+            Benchmark benchmark = new Benchmark();
+            benchmark.Count = 20;
+            benchmark.GlobalSetup();
+            var first = benchmark.FilterUsingConvolutedLinq();
+            var second = benchmark.FilterUsingCleanedUpPatternMatching();
+            foreach (var item in first)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("-------");
+
+            foreach (var item in second)
+            {
+                Console.WriteLine(item);
+            }
+#endif
+
+        }
+    }
+}
