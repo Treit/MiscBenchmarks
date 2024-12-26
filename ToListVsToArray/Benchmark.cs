1e3afc411d0ccbb72988eb61f82d37a1fb545db9
Mike Treit <mtreit@ntdev.microsoft.com>
2024-12-26 14:36:24 -0800

    ToArray vs. ToList

---
+++ ToListVsToArray/Benchmark.cs
@@ -0,0 +1,46 @@
+﻿namespace Test
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Diagnosers;
+
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        [Params(100, 999983, 1_000_000)]
+        public int Count { get; set; }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+        }
+
+        [Benchmark]
+        public int ToList()
+        {
+            return GetData().ToList().Count;
+        }
+
+        [Benchmark(Baseline = true)]
+        public int ToArray()
+        {
+            return GetData().ToArray().Length;
+        }
+
+        public IEnumerable<string> GetData()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (i % 10 == 0)
+                {
+                    yield return "";
+                }
+                else
+                {
+                    yield return i.ToString();
+                }
+            }
+        }
+    }
+}
