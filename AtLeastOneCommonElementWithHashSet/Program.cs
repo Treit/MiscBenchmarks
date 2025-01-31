103bc4f47f2b5a8911a93c96f56391185dd389d5
Mike Treit <mtreit@ntdev.microsoft.com>
2025-01-30 18:03:51 -0800

    Fix debug build

--- AtLeastOneCommonElementWithHashSet/Program.cs
+++ AtLeastOneCommonElementWithHashSet/Program.cs
@@ -13,18 +13,6 @@
             Benchmark b = new Benchmark();
             b.Count = 1000;
             b.GlobalSetup();
-            var first = b.HashSetOverlapMethod();
-            var second = b.ArrayWalkAndHashSetLookupAndHashSetContains();
-            var third = b.ArrayWalkAndHashSetLookupAndIEnumerableContains();
-            var fourth = b.ArrayWalkAndHashSetLookupAndIEnumerableContainsNoOverlap();
-            var fifth = b.ArrayWalkAndHashSetLookupAndHashSetContainsNoOverlap();
-            var sixth = b.HashSetOverlapMethodWithNoOverlap();
-            Console.WriteLine($"First: {first}");
-            Console.WriteLine($"Second: {second}");
-            Console.WriteLine($"Third: {third}");
-            Console.WriteLine($"Fourth: {fourth}");
-            Console.WriteLine($"Fifth: {fifth}");
-            Console.WriteLine($"Sixth: {sixth}");
 #endif
         }
     }
