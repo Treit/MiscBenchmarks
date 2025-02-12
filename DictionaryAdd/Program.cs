9d3aae49fb80e7589baa75f9b2810e2582062b07
Mike Treit <mtreit@ntdev.microsoft.com>
2025-02-12 13:04:39 -0800

    Update program.cs

--- DictionaryAdd/Program.cs
+++ DictionaryAdd/Program.cs
@@ -13,12 +13,6 @@
             Benchmark b = new Benchmark();
             b.Count = 1024;
             b.GlobalSetup();
-            var first = b.AddToListWithForEachLoop();
-            var second  = b.AddToListPresetCapacity();
-            var third = b.AddToListWithToListDotForEach();
-            Console.WriteLine(first);
-            Console.WriteLine(second);
-            Console.WriteLine(third);
 #endif
 
         }
