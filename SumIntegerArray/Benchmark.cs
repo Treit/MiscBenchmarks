9d90e88f0d38f4f3524fd7f11dd806b2840789a2
Mike Treit <mtreit@ntdev.microsoft.com>
2025-06-12 18:14:54 -0700

    Move to .NET 9. Simplify benchmark.

--- SumIntegerArray/Benchmark.cs
+++ SumIntegerArray/Benchmark.cs
@@ -4,110 +4,46 @@ namespace Test
 {
     using BenchmarkDotNet.Attributes;
     using BenchmarkDotNet.Diagnosers;
-    using System;
-    using System.Linq;
+        using System.Linq;
 
     [MemoryDiagnoser]
     public class Benchmark
     {
+        private int[] _ints;
+
         [Params(10, 10_000)]
         public int Count { get; set; }
 
         [GlobalSetup]
         public void GlobalSetup()
         {
+            _ints = new int[Count];
+
+            for (int i = 0; i < Count; i++)
+            {
+                _ints[i] = i;
+            }
+        }
+
+        [Benchmark]
+        public long SumUsingForLoop()
+        {
+            long result = 0;
+            var ints = _ints;
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                result += ints[i];
+            }
+
+            return result;
         }
 
         [Benchmark(Baseline = true)]
-        public long SumUsingForLoop()
-        {
-            var _ints = new int[Count];
-
-            for (int i = 0; i < Count; i++)
-            {
-                _ints[i] = i;
-            }
-            
-            long result = 0;
-
-            for (int i = 0; i < _ints.Length; i++)
-            {
-                result += _ints[i];
-            }
-
-            return result;
-        }
-
-        [Benchmark]
-        public long SumUsingForLoopSpan()
-        {
-            var _ints = new int[Count].AsSpan();
-
-            for (int i = 0; i < Count; i++)
-            {
-                _ints[i] = i;
-            }
-            
-            long result = 0;
-
-            for (int i = 0; i < _ints.Length; i++)
-            {
-                result += _ints[i];
-            }
-
-            return result;
-        }
-
-        [Benchmark]
         public long SumUsingLinqSum()
         {
-            var _ints = new int[Count];
-
-            for (int i = 0; i < Count; i++)
-            {
-                _ints[i] = i;
-            }
-
             return _ints.Sum();
         }
-
-        [Benchmark]
-        public long SumUsingRangeAndRecursionElalev()
-        {
-            var _ints = new int[Count];
-
-            for (int i = 0; i < Count; i++)
-            {
-                _ints[i] = i;
-            }
-
-            return Sum(_ints);
-        }
-
-        [Benchmark]
-        public long SumUsingRangeAndRecursionElalevWithSpan()
-        {
-            var _ints = new int[Count].AsSpan();
-
-            for (int i = 0; i < Count; i++)
-            {
-                _ints[i] = i;
-            }
-            
-            return SumSpan(_ints);
-        }
-
-        static long Sum(int[] nums) => nums switch
-        {
-              [] => 0,
-              [var firstNum, ..var restNums] => firstNum + Sum(restNums)
-        };
-
-        static long SumSpan(Span<int> nums) => nums switch
-        {
-              [] => 0,
-              [var firstNum, ..var restNums] => firstNum + SumSpan(restNums)
-        };
     }
 }
 
