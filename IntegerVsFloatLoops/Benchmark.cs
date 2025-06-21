15698d9eb36e4b7f348032863af0110e4f3340a5
Mike Treit <mtreit@ntdev.microsoft.com>
2025-06-20 17:26:44 -0700

    Loop variables

---
+++ IntegerVsFloatLoops/Benchmark.cs
@@ -0,0 +1,56 @@
+namespace IntegerVsFloatLoops
+{
+    using BenchmarkDotNet.Attributes;
+    using System;
+
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        [Params(10, 1_000_000)]
+        public int Iterations { get; set; }
+
+        [Benchmark(Baseline = true)]
+        public int IntegerLoop()
+        {
+            int count = 0;
+            for (int i = 0; i < Iterations; i++)
+            {
+                count++;
+            }
+            return count;
+        }
+
+        [Benchmark]
+        public int FloatLoop()
+        {
+            int count = 0;
+            for (float i = 0.0f; i < Iterations; i += 1.0f)
+            {
+                count++;
+            }
+            return count;
+        }
+
+        [Benchmark]
+        public int DoubleLoop()
+        {
+            int count = 0;
+            for (double i = 0.0; i < Iterations; i += 1.0)
+            {
+                count++;
+            }
+            return count;
+        }
+
+        [Benchmark]
+        public int DecimalLoop()
+        {
+            int count = 0;
+            for (decimal i = 0.0m; i < Iterations; i += 1.0m)
+            {
+                count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ IntegerVsFloatLoops/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace IntegerVsFloatLoops
+{
+    using BenchmarkDotNet.Running;
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if RELEASE
+            BenchmarkRunner.Run<Benchmark>();
+#else
+            Benchmark b = new Benchmark();
+            b.Iterations = 1000;
+
+            var result1 = b.IntegerLoop();
+            var result2 = b.FloatLoop();
+            var result3 = b.DoubleLoop();
+            var result4 = b.DecimalLoop();
+            Console.WriteLine($"Integer Loop Result: {result1}");
+            Console.WriteLine($"Float Loop Result: {result2}");
+            Console.WriteLine($"Double Loop Result: {result3}");
+            Console.WriteLine($"Decimal Loop Result: {result4}");
+#endif
+        }
+    }
+}
